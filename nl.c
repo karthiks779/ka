10742101bff7ee1c952873e2a176c7d7a453fb0e
karthiks779 <152687159+karthiks779@users.noreply.github.com>
2023-12-17 14:54:25 +0530

    Add files via upload

---
+++ nl.c
@@ -0,0 +1,25 @@
+﻿
+
+#include <stdio.h>
+#include <time.h>
+int  main()
+{   
+char a[20][20]={"gh","physics class","kannada","physics","maths","ip m","k"};
+
+	time_t c;
+time(&c);
+	
+	
+time_t rawtime;
+  struct tm * timeinfo;
+  time( &rawtime );
+  timeinfo = localtime( &rawtime );
+  
+  printf("%02d\n", timeinfo->tm_hour);
+int i=timeinfo->tm_hour%10;
+  	printf("%s",a[i]);
+ 
+
+ 		
+    return 0;
+}
